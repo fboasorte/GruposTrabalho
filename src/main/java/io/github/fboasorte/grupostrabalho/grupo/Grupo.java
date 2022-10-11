877f1c6228fef9f3d4c8586c1761d2ba94378e4d
Felipe Rocha Boa-Sorte <frb@aluno.ifnmg.edu.br>
2022-10-11 10:55:09 -0300

    BUGFIX - Compilacao para verificao de erros e conserto mappedby de grupos

--- src/main/java/io/github/fboasorte/grupostrabalho/grupo/Grupo.java
+++ src/main/java/io/github/fboasorte/grupostrabalho/grupo/Grupo.java
@@ -11,6 +11,8 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
@@ -26,6 +28,7 @@ public class Grupo implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 65)
@@ -33,7 +36,7 @@ public class Grupo implements Serializable {
 
     private Boolean ativo = true;
 
-    @OneToMany(mappedBy = "endereco",
+    @OneToMany(mappedBy = "grupo",
             cascade = CascadeType.ALL,
             orphanRemoval = true)
     private List<Atuacao> atuacoes;
